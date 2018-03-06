f8c5927a7f72fb987e5bb285972877a124521244
Moazzem hossen foyez <36882756+foyezcse@users.noreply.github.com>
2018-03-06 13:07:40 +0600

    Initial
    file added

---
+++ jlblDisplay1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Scientific_calculate;
+
+/**
+ *
+ * @author Moazzem hossen
+ */
+class jlblDisplay1 {
+
+    static void setText(String distance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
